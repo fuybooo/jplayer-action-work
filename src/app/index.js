18a3359717b5d02e1acab06111f1eb4aa88d7c87
fuybooo <fuybooo@qq.com>
2018-08-24 23:14:03 +0800

    代码优化

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,173 @@
-(() => {
-  // 页面加载完成之后执行
-  $(() => {
-    // 初始化music list
-    initMusicList();
-    // 绑定事件
-    initBindEvent();
-  });
+// 页面加载完成之后执行
+$(() => {
+  const $player = $('#imc-music-player-js');
+  const repeatType = ['list', 'random', 'single'];
+  let repeatTypeIndex = 0;
+  let currentMusic = model.musicList[0];
+  let duration = 0;
+  initMusicList();
+  initPlayer();
+  initBindEvent();
 
+  function initBindEvent() {
+    $('.imc-list-js').on('click', '.imc-list-item', function() {
+      currentMusic = model.musicList.find(item => item.id === $(this).attr('data-id'));
+      startCurrentMusic();
+      $(this).addClass('active').siblings().removeClass('active');
+    });
+    $('.imc-music-player-box-js')
+    // 暂停
+      .on('click', '.imc-ctrl-pause-js', function () {
+        $player.jPlayer('pause');
+        $('.imc-ctrl-pause-js').addClass('dn');
+        $('.imc-ctrl-play-js').removeClass('dn');
+      })
+      // 播放
+      .on('click', '.imc-ctrl-play-js', function () {
+        $player.jPlayer('play');
+        $('.imc-ctrl-play-js').addClass('dn');
+        $('.imc-ctrl-pause-js').removeClass('dn');
+      })
+      // 上一曲
+      .on('click', '.imc-ctrl-prev-js', function () {
+        getCurrentMusic('prev');
+        startCurrentMusic();
+      })
+      // 下一曲
+      .on('click', '.imc-ctrl-next-js', function () {
+        getCurrentMusic('next');
+        startCurrentMusic();
+      })
+      .on('click', '.imc-ctrl-repeat-js', function () {
+        repeatTypeIndex = (repeatTypeIndex + 1) % 3;
+        $(this).removeClass('imc-ctrl-repeat-random')
+          .removeClass('imc-ctrl-repeat-list')
+          .removeClass('imc-ctrl-repeat-single')
+          .addClass('imc-ctrl-repeat-' + repeatType[repeatTypeIndex])
+      })
+      // 跳转
+      .on('click', '.imc-progress-mask-js', function (event) {
+        const x = event.pageX || event.clientX;
+        const offset = $('.imc-progress-mask-js').offset().left;
+        const progressWidth = $('.imc-progress-mask-js').width();
+        const per = (x - offset) / progressWidth;
+        if (duration) {
+          $player.jPlayer('playHead', per * 100);
+        }
+      })
+      .on('click', '.imc-ctrl-img-js', function () {
+        $('.imc-voice-bg-js').toggleClass('dn');
+      })
+      .on('click', '.imc-voice-bar-mask-js', function (event) {
+        const y = event.pageY || event.clientY;
+        const offset = $('.imc-voice-bar-mask-js').offset();
+        $('.imc-voice-bar-js').height(100 - (y - offset.top));
+        $('.imc-dot-small-js').css('top', (y - offset.top));
+        $player.jPlayer('volume', 1 - (y - offset.top) / 100);
+      });
+    // 绑定document的点击事件，当点击到非voice-bg区域时，关闭
+    $(document).on('click.voice', function (event) {
+      if (!$('.imc-voice-bg-js').hasClass('dn') && !(
+          $(event.target).hasClass('imc-ctrl-img-js') ||
+          $(event.target).hasClass('imc-voice-bg-js') ||
+          $.contains($('.imc-voice-bg-js')[0], event.target)
+        )) {
+        $('.imc-voice-bg-js').addClass('dn');
+      }
+    });
+
+    // 播放的时候运行
+    $player.on($.jPlayer.event.timeupdate, function (event) {
+      // console.log(event.jPlayer.status);
+      duration = event.jPlayer.status.duration;
+      $('.imc-time-current-js').html($.jPlayer.convertTime(event.jPlayer.status.currentTime));
+      $('.imc-time-total-js').html($.jPlayer.convertTime(duration));
+      $('.imc-progress-current-js').width(`${event.jPlayer.status.currentPercentAbsolute}%`);
+      $('.imc-dot-big-js').css('left', `${event.jPlayer.status.currentPercentAbsolute}%`);
+    });
+  }
+
+  function initPlayer() {
+    $player.jPlayer({
+      ready: function () {
+        startCurrentMusic(true);
+      },
+      ended: function () {
+        getCurrentMusic('next');
+        startCurrentMusic();
+      },
+      swfPath: './node_modules/jplayer/dist/jplayer',
+      supplied: 'mp3',
+      wmode: 'window',
+      useStateClassSkin: true,
+      autoBlur: false,
+      smoothPlayBar: true,
+      keyEnabled: true,
+      remainingDuration: true,
+      toggleDuration: true,
+      timeFormat: {
+        showMin: true,
+        showSec: true,
+        sepMin: ':'
+      }
+    });
+  }
+
+  /**
+   * 获取接下来的music
+   * 点击上一曲 prev
+   * 点击下一曲 next
+   * @param type
+   */
+  function getCurrentMusic(type) {
+    // 找到当前music所在的位置
+    const currentIndex = model.musicList.findIndex(item => item === currentMusic);
+    // 列表循环
+    if (repeatTypeIndex === 0) {
+      let index = 0;
+      if (type === 'prev') {
+        index = currentIndex ? currentIndex - 1 : model.musicList.length - 1;
+      } else if (type === 'next') {
+        index = currentIndex === model.musicList.length - 1 ? 0 : currentIndex + 1;
+      }
+      currentMusic = model.musicList[index];
+    } else if (repeatTypeIndex === 1) {
+      if (type === 'prev') {
+        currentMusic = model.musicList[currentIndex ? currentIndex - 1 : model.musicList.length - 1];
+      } else if (type === 'next') {
+        let index = Math.floor(Math.random() * model.musicList.length);
+        while (index === currentIndex) {
+          index = Math.floor(Math.random() * model.musicList.length);
+        }
+        currentMusic = model.musicList[index];
+      }
+    }
+  }
+
+  function activateCurrent() {
+    $(`.imc-list-item[data-id=${currentMusic.id}]`).addClass('active').siblings().removeClass('active');
+  }
+
+  function startCurrentMusic(isInit) {
+    getCurrentMusicInfo();
+    activateCurrent();
+    $player.jPlayer('setMedia', {
+      title: currentMusic.name,
+      mp3: currentMusic.src
+    });
+    if (!isInit) {
+      $player.jPlayer('play')
+    }
+
+  }
+
+  function getCurrentMusicInfo() {
+    $('.imc-title-text-js').html(currentMusic.name);
+    $('.imc-artist-text-js').html(currentMusic.artist);
+    $('.imc-music-img-js').css({
+      'background-image': `url("${currentMusic.img}")`
+    });
+  }
 
   /**
    * 初始化music list
@@ -15,17 +176,14 @@
     $('.imc-list-js').empty().html(model.musicList.map((item) => `
             <li class="imc-list-item" data-id="${item.id}">
                 <div class="imc-item-name">${item.name}</div>
-                <div class="imc-item-info">${item.size} ${item.artist}${item.album ? ' · ' + item.album : ''}${item.description ? ' · ' + item.description : ''}</div>
+                <div class="imc-item-info">
+${item.size} ${item.artist}${
+      item.album ? ' · ' + item.album : ''
+    }${
+      item.description ? ' · ' + item.description : ''
+    }</div>
             </li>
         `).join(''));
   }
+});
 
-  function initBindEvent() {
-    $('.imc-list-js').on('click', '.imc-list-item', function () {
-      const musicData = model.musicList.find(item => item.id === $(this).attr('data-id'));
-      if (musicData) {
-        console.log(musicData);
-      }
-    });
-  }
-})();
--- src/app/model/model.js
+++ src/app/model/model.js
@@ -1,66 +1,56 @@
 const model = (() => {
-    return {
-        musicList: [
-            {
-                id: '1',
-                name: '忽然之间',
-                artist: '莫文蔚',
-                size: '3.2M',
-                album: '就是莫文蔚',
-                description: '',
-                img: '',
-                src: ''
-            },
-            {
-                id: '2',
-                name: '迷宫',
-                artist: '王若琳',
-                size: '4.0M',
-                album: '',
-                description: '',
-                img: '',
-                src: ''
-            },
-            {
-                id: '3',
-                name: '此时彼岸',
-                artist: '刘惜君',
-                size: '10.6M',
-                album: '我站在桥上看风景',
-                description: '',
-                img: '',
-                src: ''
-            },
-            {
-                id: '4',
-                name: '追梦赤子心',
-                artist: '鹿晗',
-                size: '11.0M',
-                album: '追梦赤子心',
-                description: '《空天猎》电影插曲',
-                img: '',
-                src: ''
-            },
-            {
-                id: '5',
-                name: '我的歌声里',
-                artist: '曲婉婷',
-                size: '3.2M',
-                album: '',
-                description: '',
-                img: '',
-                src: ''
-            },
-            {
-                id: '6',
-                name: '方圆几里',
-                artist: '薛之谦',
-                size: '4.0M',
-                album: '意外',
-                description: '',
-                img: '',
-                src: ''
-            },
-        ]
-    }
+  return {
+    musicList: [
+      {
+        id: '1',
+        name: '忽然之间',
+        artist: '莫文蔚',
+        size: '3.2M',
+        album: '就是莫文蔚',
+        description: '',
+        src: 'http://isure.stream.qqmusic.qq.com/C400000B0ypt0gL3vR.m4a?vkey=360B4997A857235C813F110A9FEC65101660F29A57CA902622C1B40D5FC3140527CE60C87938F0E151C4CA780FFB353A703A60C4A285DB6D&guid=7321117441&uin=0&fromtag=66',
+        img: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000003q2Hji2dzAyI.jpg?max_age=2592000',
+      },
+      {
+        id: '2',
+        name: '等你下课（with 杨瑞代）',
+        artist: '周杰伦',
+        size: '4.2M',
+        album: '等你下课',
+        description: '',
+        src: 'http://isure.stream.qqmusic.qq.com/C400001J5QJL1pRQYB.m4a?vkey=64F3D0674E16F30F9722F9AB0AA286D919A5E1F9F86E7F35CD384B701D9F24B9C484F4A692C82A6490ED124124A19CF29D513B898D11536F&guid=7321117441&uin=0&fromtag=66',
+        img: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000003bSL0v4bpKAx.jpg?max_age=2592000',
+      },
+      {
+        id: '3',
+        name: '你的背包',
+        artist: '陈奕迅',
+        size: '13.2M',
+        album: '你的陈奕迅',
+        description: '',
+        src: 'http://isure.stream.qqmusic.qq.com/C400004IsqcS2BilGv.m4a?vkey=2CA4ED4826DD068D72D669A4DDD12EA5B5A38551DD386A520621639C2400B3763124BC1061F627FD2EA7CFC8F838ACDE1F98689AB0797919&guid=7321117441&uin=0&fromtag=66',
+        img: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000004E0Y0A0R8FYZ.jpg?max_age=2592000',
+      },
+      {
+        id: '4',
+        name: '爱的箴言',
+        artist: '邓丽君',
+        size: '6.2M',
+        album: '邓丽君珍藏集',
+        description: '',
+        src: 'http://isure.stream.qqmusic.qq.com/C400003FRus22iPAYb.m4a?vkey=A9B4977817F4F7675DFFA02CEBBF187703C1F0037CFB0B8635D0BF9C6327F1A8F078A5B095EB4D03D39956961440A927CDC5842586D90E8A&guid=7321117441&uin=0&fromtag=66',
+        img: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000000wPGGh1SFNF5.jpg?max_age=2592000',
+      },
+      {
+        id: '5',
+        name: '演员',
+        artist: '薛之谦',
+        size: '4.4M',
+        album: '绅士',
+        description: '',
+        src: 'http://isure.stream.qqmusic.qq.com/C400001Qu4I30eVFYb.m4a?vkey=D3A37391AE7B8D4A0ACAB0D39A65DB6FF777439AC3E60C018EB270F984AED45A6DFCD0F8CC83151A291783778AFDE2330D8E17DCF1C60D66&guid=7321117441&uin=0&fromtag=66',
+        img: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000003y8dsH2wBHlo.jpg?max_age=2592000',
+      },
+    ]
+  }
 })();
